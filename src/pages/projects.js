edf4255d6180a5b06dc9578cb9e2bcc1509df602
Mark Hernandez <markedcode@gmail.com>
2018-02-06 19:47:25 -0600

    Add libraries used per project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,7 +3,14 @@ import Helmet from 'react-helmet'
 
 import './projects.less'
 
-const Project = ({ title, description, githubUrl, demoUrl, languages }) => (
+const Project = ({
+  title,
+  description,
+  githubUrl,
+  demoUrl,
+  languages,
+  libraries
+}) => (
   <article className='project'>
     <h3>{title}</h3>
 
@@ -28,8 +35,14 @@ const Project = ({ title, description, githubUrl, demoUrl, languages }) => (
     <p>
       {languages &&
         languages.map((lang, idx) => (
-          <span className='label info' key={idx}>
+          <span className='label default' key={idx}>
             {lang}
+          </span>
+        ))}
+      {libraries &&
+        libraries.map((lib, idx) => (
+          <span className='label info' key={idx}>
+            {lib}
           </span>
         ))}
     </p>
@@ -46,30 +59,42 @@ const ProjectPage = () => (
       description='Sonifies your JavaScript code'
       demoUrl='https://raikesschoolds.github.io/cse-cohen'
       languages={['JavaScript']}
+      libraries={['Acorn', 'HowlerJS', 'React', 'Redux']}
     />
     <Project
       title='Generator LionByte'
       description='A Yeoman generator for NodeJS projects'
       githubUrl='https://github.com/MarkH817/generator-lionbyte'
       languages={['JavaScript']}
+      libraries={['Yeoman-Generator']}
     />
     <Project
       title='Online Shop Application'
       description='A web application following MVC approach'
       githubUrl='https://github.com/thien0411/CSCE464-Online-Shop-Application'
       languages={['Java']}
+      libraries={['Java Servlets']}
+    />
+    <Project
+      title='Blog Single Page App'
+      description='A blog SPA that connects to a REST API'
+      githubUrl='https://github.com/MarkH817/blog-single-page-application'
+      languages={['JavaScript']}
+      libraries={['Angular', 'ExpressJS', 'MongoDB']}
     />
     <Project
       title='Pallete Ballot'
       description='An extension to store and access color palettes'
       githubUrl='https://github.com/MarkH817/palette-ballot'
       languages={['JavaScript']}
+      libraries={['React']}
     />
     <Project
       title='Sync or Swim'
       description='A simple, fast, and secure file syncing application'
       githubUrl='https://github.com/QuentinCovert/SyncOrSwim'
       languages={['Python']}
+      libraries={['SQLAlchemy', 'PyQT4', 'Watchman']}
     />
   </section>
 )
