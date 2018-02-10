a7e99447057969475cf336ecd58f055294ba75d1
Mark Hernandez <markedcode@gmail.com>
2018-02-09 22:43:19 -0600

    Modify import order of external stylesheets

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import 'prismjs/themes/prism-okaidia.css'
 
 import Header from '../components/Header'
 import Navigation from '../components/Navigation/index'
