d7e71d53377ded4395867a60f91b79907cb82b19
Mark Hernandez <mark@lion-byte.com>
2026-07-21 00:50:34 -0500

    Add `listAll` util function

--- src/loaders/at.margin.ts
+++ src/loaders/at.margin.ts
@@ -1,7 +1,6 @@
-import AT from '#data/at.json' with { type: 'json' }
 import note from '#lexicons/at/margin/note.ts'
-import { Client } from '@atproto/lex'
 import { z } from 'zod/mini'
+import { listAll } from './utils/lex-client'
 import { parseRKey } from './utils/parse-rkey'
 
 export const MarginNoteCollection = z.object({
@@ -15,25 +14,22 @@ export const MarginNoteCollection = z.object({
 })
 
 export async function getMarginAtNotes() {
-  const client = new Client(AT.pdsUrl)
-  const iterator = client.listAll(note, {
-    repo: AT.handle as `${string}.${string}`,
-  })
-
-  const results: z.infer<typeof MarginNoteCollection>[] = []
-  for await (const record of iterator) {
-    if (!note.matches(record.value)) {
-      continue
-    }
-    results.push({
-      id: parseRKey(record.uri),
-      createdAt: new Date(record.value.createdAt),
-      tags: record.value.tags ?? [],
-      title: record.value.target.title ?? '',
-      source: record.value.target.source,
-      highlightQuote: record.value.target.selector?.exact ?? null,
-      motivation: record.value.motivation,
-    })
-  }
-  return results
+  const records = await listAll(note)
+  return records.reduce<z.infer<typeof MarginNoteCollection>[]>(
+    (acc, record) => {
+      if (note.matches(record.value)) {
+        acc.push({
+          id: parseRKey(record.uri),
+          createdAt: new Date(record.value.createdAt),
+          tags: record.value.tags ?? [],
+          title: record.value.target.title ?? '',
+          source: record.value.target.source,
+          highlightQuote: record.value.target.selector?.exact ?? null,
+          motivation: record.value.motivation,
+        })
+      }
+      return acc
+    },
+    [],
+  )
 }
