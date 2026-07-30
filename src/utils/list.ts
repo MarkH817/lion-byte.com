053589bd5d549cee2f68cf97ce98a9507875f21e
Mark Hernandez <mark@lion-byte.com>
2026-07-30 18:28:19 -0500

    Update margin.at notes visual

---
+++ src/utils/list.ts
@@ -0,0 +1,21 @@
+import type { NonEmptyList } from '#types/list.ts'
+
+export function groupBy<T, V extends string | number>(
+  list: T[],
+  keyFn: (item: T) => V,
+) {
+  const map = new Map<V, NonEmptyList<T>>()
+  for (const item of list) {
+    const key = keyFn(item)
+    if (!map.has(key)) {
+      map.set(key, [item])
+    } else {
+      map.get(key)!.push(item)
+    }
+  }
+  return Array.from(map).map(([key, items]) => ({ key, items }))
+}
+
+export function unique<T>(list: T[]): T[] {
+  return Array.from(new Set(list))
+}
