00cf66cb91150c9287b60c78c145cb027e79478d
Mark Hernandez <mark@lion-byte.com>
2026-08-23 20:07:46 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,35 @@
 // @ts-check
+import sitemap, { ChangeFreqEnum } from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import og from 'astro-og'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://www.lion-byte.com',
-  integrations: [icon(), og()],
+  integrations: [
+    icon(),
+    og(),
+    sitemap({
+      lastmod: new Date(),
+      changefreq: ChangeFreqEnum.WEEKLY,
+      chunks: {
+        posts: (item) => {
+          if (/\/(blog|notes)\//.test(item.url)) {
+            item.priority = 0.7
+            return item
+          }
+        },
+      },
+      serialize: (item) => {
+        if (/\.com\/$/.test(item.url)) {
+          item.changefreq = ChangeFreqEnum.DAILY
+          item.priority = 1
+        } else if (/\/about\//.test(item.url)) {
+          item.priority = 0.7
+        }
+        return item
+      },
+    }),
+  ],
   markdown: { shikiConfig: { wrap: true, theme: 'dark-plus' } },
 })
