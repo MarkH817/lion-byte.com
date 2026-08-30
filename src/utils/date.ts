e229c3b8e6006897a548c5921df29892b1725f1e
Mark Hernandez <mark@lion-byte.com>
2026-08-30 17:29:43 -0500

    Simplify styles and add timestamp to post feed
    - Reintroduce `less` to deduplicate light/dark theme declaration
    - Add link hover color
    - Remove animations on link hovering from various components
      - Replaced by altering color and text decoration style
    - Add post publish date to `/blog/` and `/notes/` pages
    - Fix `formatDate()` config when showing both date and time

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -12,6 +12,6 @@ export function formatDate(
 ): string {
   return new Intl.DateTimeFormat(language, {
     ...(showDate ? { month: 'long', day: '2-digit', year: 'numeric' } : {}),
-    ...(showTime ? { timeStyle: 'short' } : {}),
+    ...(showTime ? { hour: '2-digit', minute: '2-digit' } : {}),
   }).format(date)
 }
